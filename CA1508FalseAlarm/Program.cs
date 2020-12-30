63f3f3750e899aae2a54d19a1b02f0fc19ee018b
Jeff Weiner <nebula@planet-nebula.com>
2020-12-29 18:51:05 -0800

    Add project files.

---
+++ CA1508FalseAlarm/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CA1508FalseAlarm
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            HashSet<int> state = new HashSet<int>
+            {
+                0,
+            };
+
+            int hits = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                hits += state.Contains(i) ? 1 : 0;
+            }
+
+            if (hits != 0)
+            {
+                Console.WriteLine($"Analyzer thinks we will never have any hits, but we had {hits}...");
+            }
+        }
+    }
+}
